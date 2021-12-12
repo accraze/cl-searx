ed40d6179dac975fcc72019525261f15e3ce4e3e
Andy Craze <accraze@gmail.com>
2021-12-12 12:44:52 -0800

    fix json parsing

--- searx.lisp
+++ searx.lisp
@@ -2,7 +2,6 @@
 
 (defun websearch (query)
   (let* (
-        ; (key (uiop:getenv "SEARCH_KEY"))
         (endpoint (uiop:getenv "SEARX_ENDPOINT"))
         (command
           (concatenate
@@ -10,22 +9,10 @@
             "curl -v -X GET \"" endpoint "?q="
             (drakma:url-encode query :utf-8)
             "&format=json\""
-            ; " -H \"Ocp-Apim-Subscription-Key: " key "\"")
         ))
         (response
             (uiop:run-program command :output :string)))
-    (print response)
-    ; (with-input-from-string
-    ;   (s response)
-    ;   (print s)
-    ; (let* ((json-as-list (json:decode-json s))
-    ;     (values (cdadr (cddr (nth 2 json-as-list)))))
-    ;   (mapcar #'(lambda (x)
-    ;               (let ((query (assoc :query x))
-    ;                    (infoboxes (assoc :infoboxes x))
-    ;                    (results (assoc :results x)))
-    ;                 (list (cdr query) (cdr infoboxes) (cdr results))))
-    ;           values)
-    ; )
-    ; )
+    (with-input-from-string
+      (s response)
+      (json:decode-json s))
 ))
